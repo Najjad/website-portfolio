472d84750df0b4cbde1b98877d3c291c6dbfe47b
Najjad <najjadzeenni19@gmail.com>
2024-05-20 21:07:32 +0400

    fix: node ver

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".DS_Store","assets/particles.json","favico.png","favicon.ico","images/.DS_Store","images/profile.png"]),
 	mimeTypes: {".json":"application/json",".png":"image/png",".ico":"image/vnd.microsoft.icon"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.5c8f6387.js","app":"_app/immutable/entry/app.274fb537.js","imports":["_app/immutable/entry/start.5c8f6387.js","_app/immutable/chunks/scheduler.c358b412.js","_app/immutable/chunks/singletons.343aa9c3.js","_app/immutable/entry/app.274fb537.js","_app/immutable/chunks/scheduler.c358b412.js","_app/immutable/chunks/index.204ce088.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.c83bf9d6.js","app":"_app/immutable/entry/app.43da1b08.js","imports":["_app/immutable/entry/start.c83bf9d6.js","_app/immutable/chunks/scheduler.c358b412.js","_app/immutable/chunks/singletons.4d68e25d.js","_app/immutable/entry/app.43da1b08.js","_app/immutable/chunks/scheduler.c358b412.js","_app/immutable/chunks/index.204ce088.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
