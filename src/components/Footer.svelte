4275c1a2fe194f81df75bd463160e682ae5f7339
Najjad Zeenni <najjadzeenni19@gmail.com>
2024-05-21 11:48:25 +0400

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,7 +2,7 @@
     class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
     <p class="px-4 py-2 bg-white text-slate-950 font-medium">
-        Connect with me &darr;
+        Hire &darr;
     </p>
     <div class="flex flex-col gap-4 items-center justify-center">
         <!-- <div
@@ -17,6 +17,6 @@
         </div>
         <p class="mx-auto text-sm opacity-70">or</p>
         <p class="">Hugh_jass@gmail.com</p> -->
-        <p><b class="pr-2">Email</b> najjadzeenni19@gmail.com</p>
+        
     </div>
 </footer>
