9e19b6c3ba50bc1ec53e6f5945620cd83839318c
Najjad Zeenni <najjadzeenni19@gmail.com>
2024-05-21 11:58:19 +0400

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,7 @@
 <footer
     class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
-    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
-        Hire &darr;
-    </p>
+
     <div class="flex flex-col gap-4 items-center justify-center">
         <!-- <div
             class="flex items-center gap-4 sm:gap-6 text-2xl sm:text-3xl md:text-4xl mx-auto"
